eb15a2a6fa58df1ad862804ce53db0e288189805
dinesh kumar <dinesh2468246@gmail.com>
2023-11-28 20:38:09 +0530

    first commit

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -8,6 +8,7 @@ const api = express();
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
+
 api.use("/api/", router);
 
 export const handler = serverless(api);
